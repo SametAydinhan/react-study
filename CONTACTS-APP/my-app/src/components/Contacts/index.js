903d127610fc89a6ab9b19147c9e24a0feebdd4a
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-03-07 14:31:48 +0300

    feat:study contact app

---
+++ CONTACTS-APP/my-app/src/components/Contacts/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import List from './List'
+import Form from './Form'
+export default function () {
+  return (
+    <div>
+        <List/>
+        <Form/>
+    </div>
+  )
+}
