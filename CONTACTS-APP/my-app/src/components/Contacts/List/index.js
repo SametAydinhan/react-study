1307eaf8138b657cf91d89427e2e48aef9f87ba0
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-03-09 11:55:00 +0300

    feat:add filtered

--- CONTACTS-APP/my-app/src/components/Contacts/List/index.js
+++ CONTACTS-APP/my-app/src/components/Contacts/List/index.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import {useState} from 'react'
 
 function List({contacts}) {
+  const [filetText,setFilterText] = useState('');
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key].toString().toLowerCase().includes(filetText.toLocaleLowerCase())
+    )
+  });
+
+
   return (
     <div>
+
+      <input placeholder='Filter contact' value={filetText} onChange={(e) => setFilterText(e.target.value)}/>
       <ul>
         {
-          contacts.map((contact, i) => 
+          filtered.map((contact, i) => 
           <li key={i}>{contact.fullname}</li> )
         }
       </ul>
     </div>
   )
-}
+  }
 
 export default List
--- CONTACTS-APP/my-app/src/components/Contacts/index.js
+++ CONTACTS-APP/my-app/src/components/Contacts/index.js
@@ -2,7 +2,18 @@ import {useState,useEffect} from 'react'
 import List from './List'
 import Form from './Form'
  function Contacts() {
-  const[contacts, setContacts] = useState([]);
+  const[contacts, setContacts] = useState([
+    {
+      fullname: 'Samet',
+      phone_number: "123123"
+    },{
+      fullname: 'Işınnur',
+      phone_number: '456456'
+    },{
+      fullname: 'Ali',
+      phone_number: '789789'
+    }
+  ]);
   
   useEffect(() => {
     console.log(contacts);
