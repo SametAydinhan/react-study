d43c10b13a18a052f2f3e02ac3449265bdd75bd5
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-05-04 13:54:19 +0300

    feat:bir kanala abone olmak

--- realtime-colors/client/src/components/Palatte.js
+++ realtime-colors/client/src/components/Palatte.js
@@ -1,10 +1,10 @@
 import {useState} from 'react'
 import { send } from '../socketApi';
-function Palatte() {
+function Palatte({activeColor}) {
   const [color,setColor] = useState('#000');
   return (
     <div className='palatte'>
-        <input type="color" value={color} onChange={(e) => setColor(e.target.value)}/>
+        <input type="color" value={activeColor} onChange={(e) => setColor(e.target.value)}/>
         <button onClick={() => send(color)}>Click</button>
        
     </div>
