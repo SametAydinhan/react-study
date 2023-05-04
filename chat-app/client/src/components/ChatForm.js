507b9387a598769a8f8f3c0612d64c9a6fa70796
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-05-04 18:28:15 +0300

    feat:temel bileşenlerin geliştirilmesi

--- chat-app/client/src/components/ChatForm.js
+++ chat-app/client/src/components/ChatForm.js
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import styles from "./styles.module.css"
+import { useState } from 'react'
 function ChatForm() {
+    const [message, setMessages] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(message);
+        setMessages("");
+    };
   return (
-    <div>ChatForm</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input className={styles.textInput} value={message} onChange={(e) => setMessages(e.target.value)}/>
+        </form>
+    </div>
   )
 }
 
