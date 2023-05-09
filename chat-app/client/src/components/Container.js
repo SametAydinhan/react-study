00698c34d2ee7c0ff34a7fae82a7fe5c316a2139
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-05-09 15:36:14 +0300

    feat:add client the right side massage

--- chat-app/client/src/components/Container.js
+++ chat-app/client/src/components/Container.js
@@ -1,11 +1,17 @@
 import {useEffect} from 'react';
 import ChatList from './ChatList';
 import ChatForm from './ChatForm';
-import { init } from '../socketApi';
+import { init,subscribeChat } from '../socketApi';
+import {useChat} from "../context/ChatContext"
 function Container() {
+  const {setMessages} = useChat();
 
   useEffect(() =>{
     init();
+
+    subscribeChat((message) => {
+      setMessages(prevState => [...prevState,{message}])
+    });
   },[])
   return (
     <div className='App'>
