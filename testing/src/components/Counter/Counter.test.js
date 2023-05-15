497a7c1faf888d0c8b527ecc6fb3981d0cc4f0a7
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-05-15 19:10:10 +0300

    feat:React Testing Library - 1

--- testing/src/components/Counter/Counter.test.js
+++ testing/src/components/Counter/Counter.test.js
@@ -1,10 +1,25 @@
 import {render,screen,fireEvent } from "@testing-library/react"
 import Counter from "./index";
 
-it("increese btn", ()=>{
-    render(<Counter/>);
-    const count = screen.getByText("0");
-    const increaseBtn = screen.getByText("Increase");
+describe("Counter Tests", () =>{
+    let increaseBtn,decreaseBtn,count;
+
+    beforeEach(()=>{
+        render(<Counter/>);
+         increaseBtn = screen.getByText("Increase");
+         decreaseBtn = screen.getByText("Decrease");
+         count = screen.getByText("0");
+    })
+
+    test("increese btn", ()=>{
     fireEvent.click(increaseBtn);
     expect(count).toHaveTextContent("1");
 });
+
+test("decreese btn", ()=>{
+    fireEvent.click(decreaseBtn);
+    expect(count).toHaveTextContent("-1");
+});
+})
+
+
